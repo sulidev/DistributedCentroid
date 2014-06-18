c2ee0512b0c48f3abc56bd836f34d6b179e4eb7e
lp2 <lp2@debian7.lp2.if.its.ac.id>
2014-06-18 12:53:15 +0700

    dispy fixing and loading data

--- jobManager.py
+++ jobManager.py
@@ -1,19 +1,53 @@
-#def compute():
+import csv
+import pickle
+from numpy import vstack,array
+from scipy.cluster.vq import kmeans,vq
+import os
 
+def computeDistance(dTestRow,dCentroid):
+    from scipy.spatial.distance import euclidean
+    jarak = []
+    for row in dCentroid:
+        jarak.append(euclidean(dTestRow,row))
+
+    jos = 999999999999
+    c = 0
+    idx = 9999
+    for s in jarak:
+        if(s < jos):
+            jos = s
+            idx = c
+        c = c + 1
+    return jos,idx
+
+def cekFile():
+    if(os.path.exists('centroids.txt')):
+        return True
+    else:
+        return False
 
 def loadData():
-    dTrain = open("data training.csv", "rb")
-    dTesting = open("data testing.csv", "rb")
-    return dTrain,dTesting
+    dTrain = []
+    dloaded = []
+    if(cekFile()!=True):
+        dTrain = open('data training.csv', 'rb')
+    else:
+        dCentroid = open('centroids.txt', 'rb')
+        dloaded = pickle.load(dCentroid)
 
-def main():
-    import dispy
-    import csv
-    from numpy import vstack,array
-    from scipy.cluster.vq import kmeans,vq
+    dTesting = open('data testing.csv', 'rb')
+    reader = csv.reader(dTesting)
+    myarr = []
+    for row in reader:
+        nrow = []
+        for col in row:
+            nrow.append(float(col))
+        myarr.append(nrow)
 
-    dTrain,dTesting = loadData()
-    reader = csv.reader(dTrain);
+    return dTrain,myarr,dloaded
+
+def computeCentroid(dTrain):
+    reader = csv.reader(dTrain)
     myarr = []
     for row in reader:
         nrow = []
@@ -23,7 +57,25 @@ def main():
 
     data = vstack(myarr)
     centroids,_ = kmeans(data,23)
-    #cluster = dispy.JobCluster(compute)
+    pickle.dump(centroids,open('centroids.txt','wb'))
+    return centroids
+
+def main():
+    import dispy
+
+    dTrain,dTesting,dCentroid = loadData()
+    if(cekFile()!=True):
+        dCentroid = computeCentroid(dTrain)
+
+    cluster = dispy.JobCluster(computeDistance, nodes=['10.151.62.21','10.151.38.67'])
+    jobs = []
+    for x in range(1000):
+        job = cluster.submit(dTesting[x],dCentroid)
+        jobs.append(job)
+
+    for job in jobs:
+        jos,idx = job()
+        print 'Distance %d in class %d' % (jos,idx)
 
 if __name__ == '__main__':
     main()
